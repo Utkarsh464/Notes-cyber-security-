a3588792086e6433f27d5bd5d1d41be65b893f6a
Utkarsh S. <utkarshsolanki776@gmail.com>
2026-06-30 01:47:44 +0530

    hasher: choose hash algo (md5/sha1/sha256/etc)

--- python/scripts/hasher.py
+++ python/scripts/hasher.py
@@ -1,5 +1,21 @@
 import hashlib
-def hasher(password):
-    return hashlib.md5(password.strip().encode()).hexdigest()
+
+algos = {
+    "md5": hashlib.md5,
+    "sha1": hashlib.sha1,
+    "sha224": hashlib.sha224,
+    "sha256": hashlib.sha256,
+    "sha384": hashlib.sha384,
+    "sha512": hashlib.sha512,
+}
+
+def hasher(algo, password):
+    return algo(password.strip().encode()).hexdigest()
+
 word = input("enter the password :")
-print(hasher(word))
+choice = input("choose algo (md5/sha1/sha224/sha256/sha384/sha512): ")
+func = algos.get(choice)
+if func is None:
+    print("unknown algo")
+else:
+    print(hasher(func, word))
